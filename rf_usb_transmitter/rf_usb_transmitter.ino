7e8b62d1f9571384db042e29ae36a144b4f8cb48
Rick Spencer <richard.linger.spencer.3@gmail.com>
2018-08-17 22:27:33 -0400

    remove debugging code

--- rf_usb_transmitter/rf_usb_transmitter.ino
+++ rf_usb_transmitter/rf_usb_transmitter.ino
@@ -6,7 +6,7 @@
 /*
 * Arduino Wireless Communication Tutorial
 *     Example 1 - Transmitter Code
-*                
+*                  
 * by Dejan Nedelkovski, www.HowToMechatronics.com
 * 
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
@@ -19,8 +19,6 @@
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
-const int led_pin = 2;
-const int red_button_pin = 3;
 boolean red_on = false;
 boolean red_button_down = false;
 
@@ -29,16 +27,16 @@ void setup() {
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
-  Serial.begin(9600);
 
+  Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available()) {
     String command = Serial.readString();
-    Serial.println("Recieved: " + command);
     char CMD[7];
     command.toCharArray(CMD, sizeof(CMD));
     radio.write(&CMD, sizeof(CMD));
   }
+
 }
