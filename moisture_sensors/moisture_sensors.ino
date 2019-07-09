f8fbfaefcc55545f8496e2d6a4ef62112b6215f6
Rick Spencer <richard.linger.spencer.3@gmail.com>
2019-07-09 09:23:41 -0400

    added ds18 code

--- moisture_sensors/moisture_sensors.ino
+++ moisture_sensors/moisture_sensors.ino
@@ -1,20 +1,26 @@
 #include<stdlib.h>
 #include <dht.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 dht DHT;
-int moisturePin1 = A0;
-int moisturePin2 =  A1;
+int moisturePin = A0;
 int dhtPin = A2;
-int moistureSenor1Reading = 0;
-int moistureSenor2Reading = 0;
 int powerPin = 8;
+int oneWirePin = 2;
 
+OneWire oneWire(oneWirePin);
+DallasTemperature sensors(&oneWire);
+  
 void setup() 
 {
   Serial.begin(9600);
 
+  sensors.begin();
+  
   pinMode(powerPin, OUTPUT);
   digitalWrite(powerPin, LOW);
+  
   Serial.println("Begin Reading");
   Serial.println("-------------");
 }
@@ -23,27 +29,31 @@ void loop()
 {
   digitalWrite(powerPin, HIGH);
   delay(10);
-  moistureSenor1Reading = analogRead(moisturePin1);
-  moistureSenor2Reading = analogRead(moisturePin2);
+  float moistureSenorReading = analogRead(moisturePin);
   
   DHT.read11(dhtPin);
-  float tempuReading = DHT.temperature;
-  Serial.println(tempuReading);
-  char tempuBuffer[15];
-  dtostrf(tempuReading,5   , 2, tempuBuffer);
-
-  String tempuString = String(tempuBuffer);
-    
-  Serial.println("sensor=moisture1:reading=" + String(moistureSenor1Reading));
-  Serial.println("sensor=moisture2:reading=" + String(moistureSenor2Reading));
-  Serial.println("sensor=temp:reading=" + tempuString);
-  /*Serial.println("sensor=humidity:reading=" + String(DHT.humidity);*/
+  float airTemperatureReading = DHT.temperature;
+  float himudityReading = DHT.humidity;
+  
+  sensors.requestTemperatures();
+  float soilTemperatureReading = sensors.getTempCByIndex(0);
+  
+  Serial.println("sensor=moisture1:reading=" + floatToString(moistureSenorReading));
+  Serial.println("sensor=temp:reading=" + floatToString(airTemperatureReading));
+  Serial.println("sensor=temp:reading=" + floatToString(himudityReading)); 
+  Serial.println("sensor=temp:reading=" + floatToString(soilTemperatureReading)); 
   
   digitalWrite(powerPin, LOW);
   delay(3000);
   /*for(int i = 0; i < 20; i++){
     delay(30000);
   }*/
+}
+
+String floatToString(float reading) {
+  char tempBuffer[15];
+  dtostrf(reading,5   , 2, tempBuffer);
+  return String(tempBuffer);
 }
 
 
