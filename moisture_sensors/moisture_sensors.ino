6d8c752aff4c3e17a321f03d71e467bf49064509
Rick Spencer <richard.linger.spencer.3@gmail.com>
2019-07-11 13:59:46 -0400

    reverted RF code

--- moisture_sensors/moisture_sensors.ino
+++ moisture_sensors/moisture_sensors.ino
@@ -1,9 +1,3 @@
-#include <SPI.h>
-#include <RF24.h>
-#include <RF24_config.h>
-#include <nRF24L01.h>
-#include <printf.h>
-
 #include<stdlib.h>
 #include <dht.h>
 #include <OneWire.h>
@@ -13,30 +7,23 @@ dht DHT;
 int moisturePin = A0;
 int dhtPin = A2;
 int lightPin = A4;
-int powerPin = 0;
+int powerPin = 4;
 int oneWirePin = 2;
 
 OneWire oneWire(oneWirePin);
 DallasTemperature sensors(&oneWire);
-RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "10101";
 
 String deviceCode = "01";
-  
+
 void setup() 
 {
   Serial.begin(9600);
 
   sensors.begin();
-   
+
   pinMode(powerPin, OUTPUT);
   digitalWrite(powerPin, LOW);
-  
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
-  
+
   Serial.println("Begin Reading");
   Serial.println("-------------");
 }
@@ -46,39 +33,37 @@ void loop()
   digitalWrite(powerPin, HIGH);
   delay(10);
   int moistureSenorReading = analogRead(moisturePin);
-  
+
   DHT.read11(dhtPin);
   int airTemperatureReading = DHT.temperature;
   int himudityReading = DHT.humidity;
-  
+
   sensors.requestTemperatures();
   int soilTemperatureReading = sensors.getTempCByIndex(0);
-  
+
   int lightReading = analogRead(lightPin);
-  
-  Serial.println(transmitReading("SM",moistureSenorReading));
-  Serial.println(transmitReading("AT",airTemperatureReading));
-  Serial.println(transmitReading("HU",himudityReading));
-  Serial.println(transmitReading("ST",soilTemperatureReading));
-  Serial.println(transmitReading("LI",lightReading));
-  
+
+  Serial.println(transmitString("SM",moistureSenorReading));
+  Serial.println(transmitString("AT",airTemperatureReading));
+  Serial.println(transmitString("HU",himudityReading));
+  Serial.println(transmitString("ST",soilTemperatureReading));
+  Serial.println(transmitString("LI",lightReading));
+
   digitalWrite(powerPin, LOW);
   delay(3000);
 }
 
-String transmitReading(String sensor, int reading) {
- String transmitString = deviceCode + sensor +  paddedString(reading);
- char r[8];
- transmitString.toCharArray(r,8);
- radio.write(&r, sizeof(r));
- return transmitString;
+String transmitString(String sensor, int reading) {
+  String transmitString = deviceCode + sensor +  paddedString(reading);
+  return transmitString;
 }
 
 String paddedString(int reading) {
   if(reading < 0) return "ERR";
-  
+
   String str = String(reading);
   if(reading < 10) str = "00"  + str;
   else if(reading < 100) str = "0" + str;
   return str;
 }
+
